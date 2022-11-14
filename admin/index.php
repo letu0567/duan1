d68d136f5a99cb87c0b1c033250b7c7e8fcaca5f
letu0567 <106290846+letu0567@users.noreply.github.com>
2022-11-14 14:00:46 +0700

    moi
    hihi

--- admin/index.php
+++ admin/index.php
@@ -5,31 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản trị Admin</title>
-    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="../view/public/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <div class="container">
-        <div class="content">
+    <div class="container_admin">
+        <div class="content_admin">
             <div class="content_left">
 
             <div class="thongtin_user">
-                <img src="" alt="">
+                <div class="thongtin_user_con">
+                <img src="../view/public/image/z3867444882970_563680b6137cf5fd44a18c201e22bd1f.jpg" width="100px" alt="">
                 <h3>Lê Tú</h3>
                 <p>Chào mừng bạn trở lại</p>
+                </div>
                 <a href="#">Quên mật khẩu</a>
                 <a href="#">Cập nhật tài khoản</a>
+                <a href="#">Đăng xuất</a>
+
+                <div class="gach_duoi">
+
+                </div>
             </div>
             <nav>
                 <ul>
-                    <li><a href="#">Thực đơn</a></li>
-                    <li><a href="#">Món ăn</a></li>
-                    <li><a href="#">Đặt món</a></li>
-                    <li><a href="#">Đặt bàn</a></li>
-                    <li><a href="#">Bình luận</a></li>
-                    <li><a href="#">Khách hàng</a></li>
-                    <li><a href="#">Đơn hàng</a></li>
-                    <li><a href="#">Thông kê</a></li>
+                    <li><a href="#"><i class="fa-solid fa-bars"></i>   Quản lý thực đơn</a></li>
+                    <li><a href="#"><i class="fa-light fa-pan-food"></i> Quản lý món ăn</a></li>
+                    <li><a href="#">Quản lý đặt món</a></li>
+                    <li><a href="#">Quản lý đặt bàn</a></li>
+                    <li><a href="#">Quản lý bình luận</a></li>
+                    <li><a href="#">Quản lý khách hàng</a></li>
+                    <li><a href="#">Quản lý đơn hàng</a></li>
+                    <li><a href="#">Quản lý thông kê</a></li>
                 </ul>
             </nav>
 
