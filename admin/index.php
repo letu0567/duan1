5333c835da63e9ccec8e15a47e9f5c13a692fce3
letu0567 <106290846+letu0567@users.noreply.github.com>
2022-11-14 11:56:57 +0700

    Update index.php
    hih

--- admin/index.php
+++ admin/index.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản trị Admin</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    ádsa
+    <div class="container">
+        <div class="content">
+            <div class="thongtin_user">
+                <img src="" alt="">
+                <h3>Lê Tú</h3>
+                <p>Chào mừng bạn trở lại</p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">Thực đơn</a></li>
+                    <li><a href="#">Món ăn</a></li>
+                    <li><a href="#">Đặt món</a></li>
+                    <li><a href="#">Đặt bàn</a></li>
+                    <li><a href="#">Bình luận</a></li>
+                    <li><a href="#">Khách hàng</a></li>
+                    <li><a href="#">Đơn hàng</a></li>
+                    <li><a href="#">Thông kê</a></li>
+                </ul>
+            </nav>
+
+        </div>
+    </div>
 </body>
 </html>
